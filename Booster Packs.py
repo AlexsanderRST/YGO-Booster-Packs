8552437f37f316be2a0bca1c987ab83b57cb4434
Alexsander Rosante <35708503+AlexsanderRST@users.noreply.github.com>
2021-06-26 11:12:16 -0300

    Add files via upload

--- Booster Packs.py
+++ Booster Packs.py
@@ -6,73 +6,97 @@ import math
 
 pygame.init()
 
+info_sample = {'name': (),
+               'cover': '',
+               '#cards': 9,
+               'head height': 20,
+               'rarity pos': 'middle',
+               'Common': (),
+               'Rare': (),
+               'Super Rare': (),
+               'Ultra Rare': (),
+               '%R': 1,
+               '%SR': 5,
+               '%UR': 12}
+
 packs_info = {0: {'name': ('Legend of', 'Blue Eyes White Dragon',),
                   'cover': '89631140',
-                  'head height': 20,
                   '#cards': 9,
-                  'Common': ('76184692', '32274490', '89091579', '40374923', '90357090',  '9159938', '77827521',
-                             '90963488',    '32864', '34460851', '36121917', '53293545', '53375573',  '2863439',
+                  'head height': 20,
+                  'rarity pos': 'middle',
+
+                  'Common': ('76184692', '32274490', '89091579', '40374923', '90357090', '9159938', '77827521',
+                             '90963488', '32864', '34460851', '36121917', '53293545', '53375573', '2863439',
                              '37313348', '75356564', '38142739', '96851799', '15401633', '57305373', '40826495',
                              '39004808', '18710707', '22910685', '85309439', '84686841', '83464209', '44287299',
                              '85705804', '87430998', '23424603', '50913601', '86318356', '22702055', '59197169',
-                             '38199696', '76103675', '46130346', '51482758', '72842870', '32452818',  '1784619',
+                             '38199696', '76103675', '46130346', '51482758', '72842870', '32452818', '1784619',
                              '36304921', '94675535', '20060230', '93553943', '56342351', '92731455', '53153481',
                              '63308047', '98818516', '98818516', '76211194', '75376965', '71407486', '33064647',
                              '85326399', '45042329', '10202894', '73051941', '33178416', '29172562', '55444629',
-                             '43500484', '16353197', '17535588', '55291359',  '4614116', '77007920', '39774685',
-                             '1557499',   '1435851', '37820550', '36607978', '77027445', '91595718', '15052462',
+                             '43500484', '16353197', '17535588', '55291359', '4614116', '77007920', '39774685',
+                             '1557499', '1435851', '37820550', '36607978', '77027445', '91595718', '15052462',
                              '46009906', '61854111', '51267887', '98252586', '25769732'
                              ),
-                  'Rare': ('37421579', '17881964',  '1641882', '85639257', '58528964', '50045299', '66788016',
+                  'Rare': ('37421579', '17881964', '1641882', '85639257', '58528964', '50045299', '66788016',
                            '83887306', '82542267', '54541900', '13039848', '33066139', '80770678', '70681994',
-                           '63102017', '11868825', '73134081',  '9293977',  '9076207', '95952802',  '7089711',
+                           '63102017', '11868825', '73134081', '9293977', '9076207', '95952802', '7089711',
                            '55144522'
                            ),
                   'Super Rare': ('45231177', '91152256', '53129443', '12580477', '4206964', '24094653', '15025844',
                                  '28279543', '72302403', '54652250'
                                  ),
-                  'Ultra Rare': ('89631140', '46986414',  '6368038', '74677425', '83764719', '8124921', '44519536',
-                                 '70903634',  '7902349', '33396948'
+                  'Ultra Rare': ('89631140', '46986414', '6368038', '74677425', '83764719', '8124921', '44519536',
+                                 '70903634', '7902349', '33396948', '39111158', '66889139'
                                  ),
-                  'Secret Rare': ('39111158', '66889139'
-                                  ),
                   '%R': 1,
                   '%SR': 5,
                   '%UR': 12,
-                  '%SctR': 31,
                   },
-
               1: {'name': ('Lightning Overdrive',),
                   'cover': '75402014',
-                  'head height': 22,
                   '#cards': 9,
+                  'head height': 22,
+                  'rarity pos': 'middle',
                   'Common': ('32164201', '68258355', '67436768', '30829071', '66192538', '92586237', '65479980',
                              '91642007', '27036706', '53535814', '80529459', '89707961', '15792576', '52296675',
                              '88685329', '15079028', '51474037', '87468732', '14957440', '49407319', '36346532',
-                              '4017398', '31002402',  '7496001',  '6374519', '69167267',  '6552971', '32056070',
-                             '68045685',  '5439384', '68223137', '94317736', '31712840', '67100549', '93595154',
+                             '4017398', '31002402', '7496001', '6374519', '69167267', '6552971', '32056070',
+                             '68045685', '5439384', '68223137', '94317736', '31712840', '67100549', '93595154',
                              '93473606', '29867611', '55262310', '28645123', '91534476', '27923575', '54927180',
-                             '11110218', '84903021', '19275188', '46660187', '76029419', '80088625',  '7984540',
+                             '11110218', '84903021', '19275188', '46660187', '76029419', '80088625', '7984540',
                              '70473293'
                              ),
                   'Rare': ('92919429', '94821366', '26914168', '40139997', '1174075', '73345237', '70389815',
                            '10497636', '53318263', '31834488'
                            ),
                   'Super Rare': ('31042659', '93708824', '29107423', '29981935', '91864689', '76524506', '12018201',
-                                  '2896663', '48285768', '47163170', '74567889', '36224040', '62623659', '34995106',
+                                 '2896663', '48285768', '47163170', '74567889', '36224040', '62623659', '34995106',
                                  '33773528', '20989253', '85847157', '12332865', '58720904', '84125619', '47504322',
                                  '83880473', '96352326', '49964567', '44478599', '9839945'
                                  ),
-                  'Ultra Rare': ( '4647954', '67314110', '28868394', '40352445', '87746184', '13735899', '75402014',
-                                 '74689476',   '952523', '66984907', '92650018', '55049722', '47882774', '54257392',
+                  'Ultra Rare': ('4647954', '67314110', '28868394', '40352445', '87746184', '13735899', '75402014',
+                                 '74689476', '952523', '66984907', '92650018', '55049722', '47882774', '54257392',
                                  '73580471'
                                  ),
-                  'Secret Rare': (),
                   '%R': 1,
                   '%SR': 2,
                   '%UR': 6,
-                  '%SctR': 0,
-                  }}
+                  },
+              2: {'name': ('Eternal Stream',),
+                  'cover': '39272762',
+                  '#cards': 3,
+                  'head height': 86,
+                  'rarity pos': 'end',
+                  'Common': ('46918794',),
+                  'Rare': ('18446701',),
+                  'Super Rare': ('4206964',),
+                  'Ultra Rare': ('39272762',),
+                  '%R': 1,
+                  '%SR': 5,
+                  '%UR': 12}
+
+              }
 
 # SFXs
 sfx_open_pack = pygame.mixer.Sound('sound/attack.wav')
@@ -81,10 +105,11 @@ sfx_next_card = pygame.mixer.Sound('sound/draw.wav')
 
 # OBJECTS
 class Card(pygame.sprite.Sprite):
-    def __init__(self, card_id, rarity='Common'):
+    def __init__(self, card_id, pack_info):
         super().__init__()
 
         self.id = card_id
+        self.rarity = self.get_rarity(pack_info)
         self.pic = pygame.image.load(f'pics/{card_id}.jpg')
         self.width, self.height = 288, 420
 
@@ -93,24 +118,24 @@ class Card(pygame.sprite.Sprite):
         self.vel = pygame.math.Vector2()
         self.anim = self.idle
 
-        if rarity != 'Common':  # make a function?
-            label = pygame.sprite.Sprite()
-            try:
-                label.image = pygame.image.load(f'pics/booster/{rarity}.png').convert_alpha()
-            except FileNotFoundError:
-                label_info = {'Rare': {'Text': 'R', 'Color': (48, 124, 243)},
-                              'Super Rare': {'Text': 'SR', 'Color': (248, 209, 0)},
-                              'Ultra Rare': {'Text': 'UR', 'Color': (13, 230, 234)},
-                              'Secret Rare': {'Text': 'SE', 'Color': (32, 32, 32)}}
-                font = pygame.font.Font('fonts/NotoSansJP-Regular.otf', 18)
-                font.set_bold(True)
-                label.image = pygame.Surface((83, 21))
-                label.image.fill(label_info[rarity]['Color'])
-                text = font.render(label_info[rarity]['Text'], True, Color('white'), label_info[rarity]['Color'])
-                text_rect = text.get_rect(center=(label.image.get_width() / 2, label.image.get_height() / 2))
-                label.image.blit(text, text_rect)
-            label.rect = label.image.get_rect(topright=self.rect.topright)
-            self.image.blit(label.image, label.rect)
+        label = pygame.sprite.Sprite()
+        try:
+            label.image = pygame.image.load(f'pics/booster/{self.rarity}.png').convert_alpha()
+        except FileNotFoundError:
+            label_info = {'Common': {'symbol': 'N', 'color': (182, 200, 203)},
+                          'Rare': {'symbol': 'R', 'color': (48, 124, 243)},
+                          'Super Rare': {'symbol': 'SR', 'color': (248, 209, 0)},
+                          'Ultra Rare': {'symbol': 'UR', 'color': (13, 230, 234)}}
+            font = pygame.font.Font('fonts/NotoSansJP-Regular.otf', 18)
+            font.set_bold(True)
+            label.image = pygame.Surface((83, 21))
+            label.image.fill(label_info[self.rarity]['color'])
+            text = font.render(label_info[self.rarity]['symbol'], True, Color('white'),
+                               label_info[self.rarity]['color'])
+            text_rect = text.get_rect(center=(label.image.get_width() / 2, label.image.get_height() / 2))
+            label.image.blit(text, text_rect)
+        label.rect = label.image.get_rect(topright=self.rect.topright)
+        self.image.blit(label.image, label.rect)
 
     def update(self):
         self.rect.center += self.vel
@@ -155,11 +180,10 @@ class Card(pygame.sprite.Sprite):
                 self.anim = self.idle
                 game.screen.card_moving = False
 
-
-def get_rarity(pack_info, card):
-    for i in ('Common', 'Rare', 'Super Rare', 'Ultra Rare', 'Secret Rare'):
-        if card in pack_info[i]:
-            return i
+    def get_rarity(self, pack_info):
+        for rarity in ('Common', 'Rare', 'Super Rare', 'Ultra Rare'):
+            if self.id in pack_info[rarity]:
+                return rarity
 
 
 class BoosterPack(pygame.sprite.Group):
@@ -210,7 +234,7 @@ class BoosterPack(pygame.sprite.Group):
 
         self.add(self.head, self.body)
 
-        # Positioning
+        # positioning
         self.rect = pygame.Rect(367, 35, 1, 1)
         self.head.rect = self.head.image.get_rect(topleft=self.rect.topleft)
         self.body.rect = self.body.image.get_rect(topleft=self.head.rect.bottomleft)
@@ -299,8 +323,6 @@ class UnpackScreen:
         self.card_moving = False
         self.open_pack = False
 
-        # self.bg_color = (33, 23, 34)
-
         # counter
         self.counter = 1
         for card in self.cards:
@@ -378,11 +400,11 @@ class UnpackScreen:
     def generate_pack(self, info):
         self.cards.empty()
 
-        # Get Cards:
         # Fill with common cards
         cards = [random.choice(info['Common']) for _ in range(info['#cards'] - 1)]
+
         # Get the rare card
-        rarity = {'Super Rare': info['%SR'], 'Ultra Rare': info['%UR'], 'Secret Rare': info['%SctR']}
+        rarity = {'Super Rare': info['%SR'], 'Ultra Rare': info['%UR']}
         probability = [i for i in list(rarity.values()) if i > 0]
         avaible_rarity = [i for i in rarity if rarity[i] > 0]
         lcm = math.lcm(*probability)
@@ -393,14 +415,22 @@ class UnpackScreen:
         lst.extend([random.choice(info['Rare']) for _ in range(lcm - len(lst))])  # if 0 Rares?
         random.shuffle(lst)
         picked_rare = random.choice(lst)
-        cards.insert(info['#cards'] // 2, picked_rare)
+
+        # Rarity pos
+        if info['rarity pos'] == 'middle':
+            cards.insert(info['#cards'] // 2, picked_rare)
+        else:
+            cards.insert(0, picked_rare)
 
         # Put cards into pack:
         for index, card_id in enumerate(cards):
-            card = Card(card_id, get_rarity(info, card_id))
+            card = Card(card_id, info)
             card.rect.center = 512, 300  # Screen center
             self.cards.add(card)
             self.cards.change_layer(card, index)
+
+        # purchase cards
+        game.purchase += cards
 
     def get_top_card(self):
         return self.cards.get_top_sprite()
@@ -420,7 +450,7 @@ class DetailScreen:
         card.image = pygame.image.load(f'pics/{card_id}.jpg')
         card_w, card_h = card.image.get_size()
         card.image = pygame.transform.smoothscale(card.image,
-                                                  (624, round(card_h/card_w * 624)))
+                                                  (624, round(card_h / card_w * 624)))
         card.rect = card.image.get_rect(midbottom=(512, 500))
         self.card = pygame.sprite.GroupSingle(card)
 
@@ -468,22 +498,34 @@ class Game:
         self.loop = True
         self.events = pygame.event.get()
 
-        self.screens = {'unpack': UnpackScreen(0),
-                        'choose': SelectionScreen(),
-                        'detail': DetailScreen('3611830')}
-        self.screen = self.screens['choose']
+        self.screens = {}
+        self.screen = None
+
+        self.purchase = []
 
     def run(self):
+        self.screens['choose'] = SelectionScreen()
+        self.screen = self.screens['choose']
         while self.loop:
             self.event_check()
             self.screen.update()
             self.screen.draw(self.display)
             pygame.display.update()
             self.clock.tick(60)
+        self.save_purchase()
         pygame.quit()
 
     def event_check(self):
         self.events = pygame.event.get()
+
+    def save_purchase(self):
+        with open('deck/Booster Packs.ydk', 'w') as deck:
+            deck.write('#Created by AlexsanderRosante\n')
+            deck.write('#main\n')
+            for card in self.purchase:
+                deck.write(card + '\n')
+            deck.write('#extra\n')
+            deck.write('!side\n')
 
 
 if __name__ == '__main__':
